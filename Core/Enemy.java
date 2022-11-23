e8aa0f73b43098ea885790ef677598f529a8d9af
Sigfreddd <viii.gram@gmail.com>
2022-11-23 23:28:05 +0100

    ptdr c'est peu je sais mais JE REFLECHIS FJQFKOPJPKVBOJRZ

---
+++ Core/Enemy.java
@@ -0,0 +1,16 @@
+package Core;
+
+public class Enemy extends Character
+{
+	
+	public Enemy (String name, int healthPoints, int attack)
+	{
+		super(name, healthPoints, attack);
+	}
+	
+	@Override 
+	public void die()
+	{
+		
+	}
+}
