94c4f394672240309054dfc2a2c2f80e8329915c
Ellecknia <117731607+Ellecknia@users.noreply.github.com>
2022-11-24 16:10:41 +0100

    ui

--- Core/Weapon.java
+++ Core/Weapon.java
@@ -3,14 +3,24 @@ package Core;
 public abstract class Weapon extends Item implements Use{
 	private int bonusAttackPoint;
 	private int numberOfUse;
+
 	
 	public Weapon(String name, int atkpt, int life, String description) {
 		super(name, description);
 		this.setBonusAttackPoint(atkpt);
 		this.numberOfUse = life;
 	}
-	
 
+
+	public int getBonusAttackPoint() {
+		return bonusAttackPoint;
+	}
+
+	public void setBonusAttackPoint(int bonusAttackPoint) {
+		this.bonusAttackPoint = bonusAttackPoint;
+	}
+	
+	
 	@Override
 	public boolean Use() {
 		if (numberOfUse==0) {
@@ -21,16 +31,4 @@ public abstract class Weapon extends Item implements Use{
 			return true;
 		}
 	}
-
-
-	public int getBonusAttackPoint() {
-		return bonusAttackPoint;
-	}
-
-
-	public void setBonusAttackPoint(int bonusAttackPoint) {
-		this.bonusAttackPoint = bonusAttackPoint;
-	}
-	
-	
 }
