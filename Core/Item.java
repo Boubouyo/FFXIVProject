be68cb9a322fca1b802d8df4f27329648ce6c875
Ellecknia <117731607+Ellecknia@users.noreply.github.com>
2022-11-24 21:23:43 +0100

    jsp oskour g tout k c

--- Core/Item.java
+++ Core/Item.java
@@ -3,14 +3,10 @@ package Core;
 
 public abstract class Item implements Look{
 	private final String name;
-<<<<<<< Updated upstream
 	private String description;
 	private Location currentLocation;
 	private Hero hero;
-=======
-	private String description = "It's an item. It could be usefull";
 	private boolean pickable;
->>>>>>> Stashed changes
 
 	
 	// -------------------------------------------------------------------//
@@ -25,6 +21,22 @@ public abstract class Item implements Look{
 		return this.name;
 	}
 	
+	public Hero getHero() {
+		return this.hero;
+	}
+	
+	public void setHero(Hero hero) {
+		this.hero = hero;
+	}
+	
+	public Location getLocation() {
+		return this.currentLocation;
+	}
+	
+	public void setLocation(Location loc) {
+		this.currentLocation = loc;
+	}
+	
 	
 	// -------------------------------------------------------------------//
 	@Override
@@ -32,34 +44,15 @@ public abstract class Item implements Look{
 		System.out.println("You are looking at "+this.name+".");
 		System.out.println(this.description);
 	}
-<<<<<<< Updated upstream
-	
-	public Hero getHero()
-	{
-		return this.hero;
-	}
-	
-	public void setHero(Hero hero)
-	{
-		this.hero = hero;
-	}
-	
-	public Location getLocation()
-	{
-		return this.currentLocation;
-	}
-	
-	public void setLocation(Location loc)
-	{
-		this.currentLocation = loc;
-=======
 
+	
+	@Override
 	public boolean use() {
 		return false;
 	}
-
+	
+	@Override
 	public boolean take() {
 		return false;
->>>>>>> Stashed changes
 	}
 }
