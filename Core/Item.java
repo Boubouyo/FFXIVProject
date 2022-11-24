2f1b14b2590adf54d301a8ce474cc4db932c0c70
Ellecknia <anais.penot.pro@gmail.com>
2022-11-24 02:15:23 +0100

    Update Item.java
    ajout de la description?
    ajout de la méthode look?

--- Core/Item.java
+++ Core/Item.java
@@ -3,14 +3,25 @@ package Core;
 
 public class Item {
 	private final String name;
-	private final String description;
+	private String description = "It's an item. It could be usefull";
 	
 
 	public Item(String name) {
 		this.name = name;
+		this.description = initDescription();
+	}
+	
+	public String initDescription() {
+		return "hey this is a description of an item but im too stupid to do something smart sorry";
 	}
 	
 	public String getName() {
 		return this.name;
+	}
+	
+	public void look()
+	{
+		System.out.println("You are looking at "+this.name+".");
+		System.out.println(this.description);
 	}
 }
