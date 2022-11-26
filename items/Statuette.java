0b92939f4723751401373b142749f41a4eecdec0
Ellecknia <117731607+Ellecknia@users.noreply.github.com>
2022-11-26 17:30:19 +0100

    statuette repared?

--- items/Statuette.java
+++ items/Statuette.java
@@ -9,8 +9,8 @@ public class Statuette extends Pickable implements UseOn{
 	
 	
     // --------------------------CONSTRUCTEUR----------------------------------//
-	public Statuette(String name, int ID, String description, Location location) {
-		super(name, description, location, false);
+	public Statuette(String name, int ID, String description, Location location, Boolean isPickable) {
+		super(name, description, location, isPickable);
 
 		this.statuetteID = ID;
 	}
