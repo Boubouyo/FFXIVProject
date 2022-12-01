f14fcc1e16bed5b83cf73255acd16c6f1ad5eca0
Sigfreddd <viii.gram@gmail.com>
2022-12-01 15:58:36 +0100

    fix

--- Main.java
+++ Main.java
@@ -8,6 +8,8 @@ public class Main {
 
 	/**
 	 * Constructor method for the class Main.
+	 * @param args : not used in this project
+	 * @throws Exception not used in main
 	 */
 	public static void main(String[] args) throws Exception {
 		Game.gameStart();
